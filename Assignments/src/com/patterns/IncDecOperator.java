62d66003d7078219fc1d9e14abc89d0a85d9a7bc
Ramya <ramyakpass@gmail.com>
2025-03-13 22:49:54 -0400

    Commit
    Pattern triangle programs added

---
+++ Assignments/src/com/patterns/IncDecOperator.java
@@ -0,0 +1,12 @@
+package com.patterns;
+
+public class IncDecOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=3,b=2,c=5;
+int d= c++ + --b + a-- + ++c + b-- + ++a - --a + --c + ++b + a++ + --a;
+System.out.println(d);
+	}
+
+}
---
+++ Assignments/src/com/patterns/PatternStar.java
@@ -0,0 +1,137 @@
+package com.patterns;
+
+public class PatternStar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//pattern1();
+		//patternreverse();
+		//patternstar1();
+		//patternstar2();
+		//patternstar3();
+		//patternstar4();
+		//patternstar5();
+		patternstar6();
+
+}
+	
+
+	private static void patternstar6() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=5;row++) {
+			for(int space=1;space<=row;space++) {
+				System.out.print("");
+			}
+			for(int num=1; num<=6-row;num++) {
+//				System.out.print(1+" ");
+//				System.out.print(num+" ");
+//				System.out.print(row+" ");
+				System.out.print(row*5+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+
+	private static void patternstar5() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=5;row++) {
+			for(int space=1;space<=row;space++) {
+				System.out.print("  ");
+			}
+			for(int num=1; num<=6-row;num++) {
+				System.out.print(1+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+
+	private static void patternstar4() {
+		// TODO Auto-generated method stub
+		  for(int row=1;row<=5;row++) {
+			  for(int space=1;space<=5-row;space++) {
+				  System.out.print(" ");
+			  }
+			  for(int num=1; num<=row;num++) {
+//				  System.out.print(num+" ");
+//				  System.out.print(row+" ");
+				  System.out.print(row*num+" ");
+			  }
+			  System.out.println(" "); 
+		  }
+	}
+
+
+	private static void patternstar3() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=5;row++) {
+			for(int space=1;space<=5-row;space++) {
+				System.out.print(" ");
+			}
+				for(int num=1;num<=row;num++) {
+					System.out.print("1 ");
+				}
+			
+			System.out.println(" ");
+		}
+	}
+
+
+	private static void patternstar2() {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=6-i;k++) {
+				System.out.print(k+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+
+	private static void patternstar1() {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=5;j>=i;j--) 
+			{
+				System.out.print("1 ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+
+	private static void patternreverse() {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+//			for(int j=5;j>=i;j--) 
+			for(int j=1;j<=6-i;j++){
+				System.out.print("1 ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+
+	private static void pattern1() {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			for(int k=1;k<=5;k++) {
+			System.out.print(1);
+			}
+			System.out.println(" ");
+	}
+	}
+}
