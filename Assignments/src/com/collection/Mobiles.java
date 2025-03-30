d89ca0e990204e60ed40bf27336cb7e899b300be
Ramya <ramyakpass@gmail.com>
2025-03-29 23:40:39 -0400

    Collection Comparable

---
+++ Assignments/src/com/collection/Mobiles.java
@@ -0,0 +1,13 @@
+package com.collection;
+
+public class Mobiles {
+String brand;
+int price,ram;
+public Mobiles(String brand, int price, int ram) {
+	super();
+	this.brand = brand;
+	this.price = price;
+	this.ram=ram;
+}
+
+}
