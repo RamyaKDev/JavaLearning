012625992c6294ced370c27ed399d0d4645c8aa7
Ramya <ramyakpass@gmail.com>
2025-05-23 19:30:10 -0400

    Reversing string using StringBuilder

---
+++ Interview/src/com/string/Reverse_using_StringBuilder.java
@@ -0,0 +1,11 @@
+package com.string;
+
+public class Reverse_using_StringBuilder {
+
+	public static void main(String[] args) {
+	String str="hello";
+	StringBuilder rev=new StringBuilder(str);
+	System.out.println(rev.reverse());
+	}
+
+}
